59c817de85ef1dc9bda65c273306b2e8d2c0a63a
Tibo Ulens <ulens.tibo@gmail.com>
2022-02-25 22:15:06 +0100

    attempt to implement websockets

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,104 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
+use actix::{Actor, ActorContext, StreamHandler};
 use actix_web::middleware::Logger;
-use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
 use framebuffer::Framebuffer;
 
 struct AppState {
 	line_length:     u32,
 	bytes_per_pixel: u32,
 	frame:           Arc<Mutex<Vec<u8>>>,
+}
+
+impl Actor for AppState {
+	type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for AppState {
+	fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+		let msg = match msg {
+			Err(_) => {
+				ctx.stop();
+				return;
+			},
+			Ok(msg) => msg,
+		};
+
+		match msg {
+			ws::Message::Ping(msg) => ctx.pong(&msg),
+			ws::Message::Pong(_) => (),
+			ws::Message::Text(txt) => {
+				let m = txt.trim();
+				// Message format:
+				// x y r g b
+				// eg.
+				// 10 10 255 120 120 -> set (10, 10) to #FF7878
+				let parts = m.split(' ').collect::<Vec<&str>>();
+
+				if parts.len() != 5 {
+					ctx.text("ERROR (bad format)
+						Message format is `x y r g b`
+						Where:
+						  - x: int
+						  - y: int
+						  - r: int8
+						  - g: int8
+						  - b: int8
+						");
+					return;
+				}
+
+				let x = parts[0].parse::<u32>().unwrap();
+				let y = parts[1].parse::<u32>().unwrap();
+				let r = parts[2].parse::<u8>().unwrap();
+				let g = parts[3].parse::<u8>().unwrap();
+				let b = parts[4].parse::<u8>().unwrap();
+
+				let start_index =
+					(y * self.line_length + x * self.bytes_per_pixel) as usize;
+
+				let mut frame = self.frame.lock().unwrap();
+
+				if start_index + 2 < frame.len() {
+					frame[start_index] = b;
+					frame[start_index + 1] = g;
+					frame[start_index + 2] = r;
+					ctx.text("OK");
+				} else {
+					ctx.text("ERROR (out of bounds)");
+				}
+			},
+			ws::Message::Binary(_) => ctx.text("unexpected binary"),
+			ws::Message::Close(reason) => {
+				ctx.close(reason);
+				ctx.stop();
+			},
+			ws::Message::Continuation(_) => {
+				ctx.stop();
+			},
+			ws::Message::Nop => (),
+		}
+	}
+}
+
+#[get("/ws")]
+async fn get_ws(
+	req: HttpRequest,
+	stream: web::Payload,
+	data: web::Data<AppState>,
+) -> Result<HttpResponse, Error> {
+	ws::start(
+		AppState {
+			line_length:     data.line_length,
+			bytes_per_pixel: data.bytes_per_pixel,
+			frame:           Arc::clone(&data.frame),
+		},
+		&req,
+		stream,
+	)
 }
 
 #[post("/{x}/{y}/{r}/{g}/{b}")]
@@ -73,6 +163,7 @@ async fn main() -> std::io::Result<()> {
 				bytes_per_pixel,
 				frame: frame.clone(),
 			}))
+			.service(get_ws)
 			.service(set_pixel)
 	})
 	.bind(("0.0.0.0", 8000))?
